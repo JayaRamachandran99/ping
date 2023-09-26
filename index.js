d123fdfccef36f8b5698e9ed8537f2b1e43b25f2
JayaRamachandran99 <chandranvutukuri@gmail.com>
2023-09-26 22:14:52 +0530

    checking node

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// const message = <h1 id='hello'> Hi i am sai</h1> == if we want to write this jsx into react we need 
+// to write as below
+
+
+// JSX ==> const message = <h1 id='hello'> Hi i am sai</h1>
+// React==> ReactDOM.render(React.createElement('h1',{id:'hello'},'Hi i am sairam'), document.getElementById("root")) 
+
+const message = <h1 id='hello'> Hi i am sai</h1>
+ReactDOM.render(React.createElement('h1',{id:'hello'},'Hi i am sairam'), document.getElementById("root")) 
